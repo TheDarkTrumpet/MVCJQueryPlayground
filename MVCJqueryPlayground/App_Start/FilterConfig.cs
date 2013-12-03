f90cec3b30b4a1c5979a0aad4420618685e18397
David <dthole@gmail.com>
2013-12-03 15:47:53 -0600

    initial commit of project generation

---
+++ MVCJqueryPlayground/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCJqueryPlayground
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVCJqueryPlayground/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCJqueryPlayground.MvcApplication" Language="C#" %>
