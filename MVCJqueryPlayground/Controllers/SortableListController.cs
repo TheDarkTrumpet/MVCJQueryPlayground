59d7fe4b98af326d18ccb12b872f23a0fe221626
David Thole <dthole@gmail.com>
2013-12-04 10:51:01 -0600

    Adding a 'sortable list' jquery portion, at least the stub for it.

---
+++ MVCJqueryPlayground/Controllers/SortableListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCJqueryPlayground.Controllers
+{
+    public class SortableListController : Controller
+    {
+        //
+        // GET: /SortableList/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
