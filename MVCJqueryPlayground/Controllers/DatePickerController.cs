c7a1fc9d3780f8803a1c5065b07da6aa962b0dff
David Thole <dthole@gmail.com>
2013-12-06 12:55:19 -0600

    Changing the base layout to call jquery's javascript source file directory.  Also created a simple 'date picker' demo, and removed the current jquery from loading

---
+++ MVCJqueryPlayground/Controllers/DatePickerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace MVCJqueryPlayground.Controllers
+{
+    public class DatePickerController : Controller
+    {
+        //
+        // GET: /DatePicker/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
